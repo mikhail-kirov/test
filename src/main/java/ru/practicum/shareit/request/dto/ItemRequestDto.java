d66f94160f9696dab45cacea5dd6720b5ee8f339
Mikhail <mikhail.khodyrev.survai@gmail.com>
2024-07-22 22:49:04 +0300

    add-bookings_v1_0

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -2,15 +2,11 @@ package ru.practicum.shareit.request.dto;
 
 import lombok.Builder;
 import lombok.Data;
-import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
 
-import java.util.List;
 
 @Data
 @Builder
 public class ItemRequestDto {
     private User sourceRequestUser;
-    private String request;
-    private List<Item> resultItems;
 }
