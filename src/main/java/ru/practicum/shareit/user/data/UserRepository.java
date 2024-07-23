4d938894406927c2cd1422791789d9887784b03c
Mikhail <mikhail.khodyrev.survai@gmail.com>
2024-07-23 22:55:25 +0300

    add-bookings_v1_8

--- src/main/java/ru/practicum/shareit/user/data/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/data/UserRepository.java
@@ -1,10 +1,8 @@
 package ru.practicum.shareit.user.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 import ru.practicum.shareit.user.model.User;
 
 
-@Component
 public interface UserRepository extends JpaRepository<User, Long> {
 }
